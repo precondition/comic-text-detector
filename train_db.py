48807833aca4b535ba23ac628ee923bd73126873
precondition <57645186+precondition@users.noreply.github.com>
2024-01-18 14:50:13 +0100

    Check if batch[key] has attribute cuda before calling

--- train_db.py
+++ train_db.py
@@ -47,7 +47,8 @@ def eval_model(model: nn.Module, val_loader, post_process, metric_cls, criterion
             with amp.autocast():
                 for key in batch.keys():
                     if CUDA:
-                        batch[key] = batch[key].cuda()
+                        if hasattr(batch[key], "cuda"):
+                            batch[key] = batch[key].cuda()
                 preds = model(batch['imgs'])
                 metric = criterion(preds, batch, use_bce)
             loss = metric["loss"]
